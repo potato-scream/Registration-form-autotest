af26224389d30fd9c1e0a8e9af83ece39004f6b9
kreng <kr.englesson@gmail.com>
2025-08-19 17:52:33 +0100

    added config and properties

--- src/test/java/pages/components/CheckResultsComponent.java
+++ src/test/java/pages/components/CheckResultsComponent.java
@@ -6,7 +6,6 @@ import static com.codeborne.selenide.Selenide.$;
 public class CheckResultsComponent {
 
     public void checkResults(String key, String value) {
-       // $(".table-responsive").$(byText(key)).parent().shouldHave(text(value));
         $(".table-responsive").$(byText(key)).sibling(0).shouldHave(text(value));
     }
 }
