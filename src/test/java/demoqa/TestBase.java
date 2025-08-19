02c5a7311f9834cebe489d5412ae87e5eeeb74a1
kreng <kr.englesson@gmail.com>
2025-08-19 17:52:26 +0100

    added config and properties

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -1,54 +1,31 @@
 package demoqa;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
+import config.ConfigApplier;
 import helpers.Attach;
 
-import java.util.Map;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 
 public class TestBase {
+
     @BeforeAll
     static void beforeAll() {
-        // Basic browser setup
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("version", "120.0");
-        Configuration.browserSize = System.getProperty("size","1920x1080");
-        Configuration.pageLoadStrategy = "eager";
-
-        // Remote driver setup that reads from Jenkins parameters
-        String selenoidUrl = System.getProperty("selenoidUrl");
-        if (selenoidUrl != null && !selenoidUrl.isEmpty()) {
-            String user = System.getProperty("user");
-            String password = System.getProperty("password");
-            Configuration.remote = "https://" + user + ":" + password + "@" + selenoidUrl;
-        }
-
-        // Selenoid capabilities
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-
-        // Allure listener
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        new ConfigApplier().applyConfig();
+        SelenideLogger.addListener("AllureSelenide",
+                new AllureSelenide()
+                        .includeSelenideSteps(true)
+                        .screenshots(false)
+                        .savePageSource(false)
+        );
     }
 
     @AfterEach
-    void addAttachments() {
+    void attachArtifacts() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();
     }
 }
-
-
-
